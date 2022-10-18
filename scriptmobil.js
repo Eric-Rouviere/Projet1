42f8fc030b110ba16d100e08cfb228169a3292c7
Eric Rouviere <eric.rouviere2@sncf.fr>
2022-10-18 10:30:50 +0200

    import new pictures and videos

---
+++ scriptmobil.js
@@ -0,0 +1,49 @@
+function shonen() {
+    var div = document.getElementById("display_shonen");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+
+function seinen() {
+    var div = document.getElementById("display_seinen");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  function shojo() {
+    var div = document.getElementById("display_shojo");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  function artbooks() {
+    var div = document.getElementById("display_artbooks");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  function videos() {
+    var div = document.getElementById("display_videos");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+  function liseuse() {
+    var div = document.getElementById("display_liseuse");
+    if (div.style.display === "none") {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
