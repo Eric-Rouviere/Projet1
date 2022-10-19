5ec3b47ff776e325c079ace4f2a7eb74de9cd55f
Eric Rouviere <eric.rouviere2@sncf.fr>
2022-10-19 22:16:59 +0200

    caroussel ok videos design ok artbooks ok all buttons works

--- scriptmobil.js
+++ scriptmobil.js
@@ -1,3 +1,12 @@
+function auteur() {
+  var div = document.getElementById("display_auteur");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
+
 function shonen() {
     var div = document.getElementById("display_shonen");
     if (div.style.display === "none") {
@@ -31,7 +40,241 @@ function seinen() {
       div.style.display = "none";
     }
   }
-  function videos() {
+    
+  function artbooks_size1() {
+    var div = document.getElementById("size1");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size2() {
+    var div = document.getElementById("size2");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size3() {
+    var div = document.getElementById("size3");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size4() {
+    var div = document.getElementById("size4");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size5() {
+    var div = document.getElementById("size5");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size6() {
+    var div = document.getElementById("size6");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size7() {
+    var div = document.getElementById("size7");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size8() {
+    var div = document.getElementById("size8");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size9() {
+    var div = document.getElementById("size9");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size10() {
+    var div = document.getElementById("size10");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size11() {
+    var div = document.getElementById("size11");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size12() {
+    var div = document.getElementById("size12");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size13() {
+    var div = document.getElementById("size13");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size14() {
+    var div = document.getElementById("size14");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size15() {
+    var div = document.getElementById("size15");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size16() {
+    var div = document.getElementById("size16");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size17() {
+    var div = document.getElementById("size17");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size18() {
+    var div = document.getElementById("size18");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size19() {
+    var div = document.getElementById("size19");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size20() {
+    var div = document.getElementById("size20");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+  function artbooks_size21() {
+    var div = document.getElementById("size21");
+    console.log(div,"div");
+    if (div.style.display !== "none") {
+      div.style.display = "none";
+      console.log("if");
+    } else {
+      div.style.display = "block";
+      console.log("else");
+    }    
+  }
+
+      
+   function videos() {
     var div = document.getElementById("display_videos");
     if (div.style.display === "none") {
       div.style.display = "block";
@@ -47,3 +290,285 @@ function seinen() {
       div.style.display = "none";
     }
   }
+  function artbooks_size_fermeture1() {
+    var div = document.getElementById("size1");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture2() {
+    var div = document.getElementById("size2");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture3() {
+    var div = document.getElementById("size3");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture4() {
+    var div = document.getElementById("size4");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture5() {
+    var div = document.getElementById("size5");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture6() {
+    var div = document.getElementById("size6");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture7() {
+    var div = document.getElementById("size7");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture8() {
+    var div = document.getElementById("size8");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture9() {
+    var div = document.getElementById("size9");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture10() {
+    var div = document.getElementById("size10");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture11() {
+    var div = document.getElementById("size11");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture12() {
+    var div = document.getElementById("size12");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture13() {
+    var div = document.getElementById("size13");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture14() {
+    var div = document.getElementById("size14");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture15() {
+    var div = document.getElementById("size15");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture16() {
+    var div = document.getElementById("size16");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture17() {
+    var div = document.getElementById("size17");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture18() {
+    var div = document.getElementById("size18");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture19() {
+    var div = document.getElementById("size19");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture20() {
+    var div = document.getElementById("size20");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+  function artbooks_size_fermeture21() {
+    var div = document.getElementById("size21");
+    console.log(div,"div");
+    if (div.style.display !== "block") {
+      div.style.display = "block";
+      console.log("if");
+    } else {
+      div.style.display = "none";
+      console.log("else");
+    }  
+  }
+/*................................slider....................................*/
+
+var slide = new Array("./images/jaquettes/air_gear.jpg", "./images/scans/air_gear1.jpg", "./images/scans/air_gear2.jpg", 
+  "./images/scans/air_gear3.png","./images/scans/air_gear4.png","./images/scans/air_gear5.jpg");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+function fermeture_auteur() {
+  var x = document.getElementById("display_auteur");
+  x.style.display = "none";
+}
+function fermeture_shonen() {
+  var x = document.getElementById("display_shonen");
+  x.style.display = "none";
+}
+function fermeture_seinen() {
+  var x = document.getElementById("display_seinen");
+  x.style.display = "none";
+}
+function fermeture_shojo() {
+  var x = document.getElementById("display_shojo");
+  x.style.display = "none";
+}
+
+function fermeture_liseuse() {
+  var x = document.getElementById("display_liseuse");
+  x.style.display = "none";
+}
+function fermeture_videos() {
+  var x = document.getElementById("display_videos");
+  x.style.display = "none";
+}
+function fermeture_artbooks() {
+  var x = document.getElementById("display_artbooks");
+  x.style.display = "none";
+}
+function slider() {
+  var div = document.getElementById("slider");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
